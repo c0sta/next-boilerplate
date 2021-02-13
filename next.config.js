eb35772a2c8d38ac9256a708de4f90ccc9c87595
GabrielCM <gabrielcostamoura1@gmail.com>
2021-02-12 22:01:27 -0300

    :memo: Adding documentation equal

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  // other next config
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
